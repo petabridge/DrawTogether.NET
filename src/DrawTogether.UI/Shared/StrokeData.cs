79ae365bb030bb0a2c2e9ee0bf9faca2bc9c6ed4
Aaron Stannard <aaron@petabridge.com>
2021-06-03 20:48:06 -0500

    added first paint surface

---
+++ src/DrawTogether.UI/Shared/StrokeData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawTogether.UI.Shared
+{
+    public struct StrokeData
+    {
+        public int cursorSize { get; set; }
+        public double cX { get; set; }
+        public double cY { get; set; }
+        public string color { get; set; }
+    }
+}
