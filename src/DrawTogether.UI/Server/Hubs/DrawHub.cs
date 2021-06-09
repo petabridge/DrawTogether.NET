bd22d481f8ef193c44a28494f591f7f86f6edb14
Aaron Stannard <aaron@petabridge.com>
2021-06-08 19:41:22 -0500

    adding SignalR and Akka.NET infrastructure

---
+++ src/DrawTogether.UI/Server/Hubs/DrawHub.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace DrawTogether.UI.Server.Hubs
+{
+    public sealed class DrawHub : Hub
+    {
+        /// <summary>
+        /// Connects this SignalR User to a paint session in progress.
+        /// </summary>
+        /// <param name="sessionId">The unique id of a specific paint session.</param>
+        public void JoinSession(string sessionId)
+        {
+            // need to have some sort of service handle here for sending / retrieving state
+        }
+    }
+}
---
+++ src/DrawTogether.UI/Shared/Connectivity/PaintSessionProtocol.cs
@@ -0,0 +1,23 @@
+﻿namespace DrawTogether.UI.Shared.Connectivity
+{
+    public static class PaintSessionProtocol
+    {
+        public interface IPaintSessionMessage
+        {
+            /// <summary>
+            /// Refers to a unique painting instance.
+            /// </summary>
+            string InstanceId { get; }
+        }
+
+        public sealed class JoinPaintSession : IPaintSessionMessage
+        {
+            public JoinPaintSession(string instanceId)
+            {
+                InstanceId = instanceId;
+            }
+
+            public string InstanceId { get; }
+        }
+    }
+}
