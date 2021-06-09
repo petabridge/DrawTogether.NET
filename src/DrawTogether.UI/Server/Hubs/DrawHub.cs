1acd81e0e7daa5e71703d84a895adfe5a9293d12
Aaron Stannard <aaron@petabridge.com>
2021-06-08 20:10:36 -0500

    added PaintInstanceActor

--- src/DrawTogether.UI/Server/Hubs/DrawHub.cs
+++ src/DrawTogether.UI/Server/Hubs/DrawHub.cs
@@ -3,12 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using DrawTogether.UI.Server.Services;
+using DrawTogether.UI.Shared;
 using Microsoft.AspNetCore.SignalR;
 
 namespace DrawTogether.UI.Server.Hubs
 {
     public sealed class DrawHub : Hub
     {
+        private readonly IDrawSessionHandler _sessionHandler;
+
+        public DrawHub(IDrawSessionHandler sessionHandler)
+        {
+            _sessionHandler = sessionHandler;
+        }
+
         /// <summary>
         /// Connects this SignalR User to a paint session in progress.
         /// </summary>
--- src/DrawTogether.UI/Shared/Connectivity/PaintSessionProtocol.cs
+++ src/DrawTogether.UI/Shared/Connectivity/PaintSessionProtocol.cs
@@ -1,4 +1,7 @@
-﻿namespace DrawTogether.UI.Shared.Connectivity
+﻿using System.Collections.Generic;
+using Akka.Actor;
+
+namespace DrawTogether.UI.Shared.Connectivity
 {
     public static class PaintSessionProtocol
     {
@@ -12,12 +15,57 @@
 
         public sealed class JoinPaintSession : IPaintSessionMessage
         {
-            public JoinPaintSession(string instanceId)
+            public JoinPaintSession(string instanceId, string connectionId)
             {
                 InstanceId = instanceId;
+                ConnectionId = connectionId;
             }
 
             public string InstanceId { get; }
+
+            /// <summary>
+            /// The unique connection id for this websocket in SignalR
+            /// </summary>
+            public string ConnectionId { get; }
         }
+
+        public sealed class AddStrokes : IPaintSessionMessage
+        {
+            public AddStrokes(string instanceId, IReadOnlyList<StrokeData> strokes)
+            {
+                InstanceId = instanceId;
+                Strokes = strokes;
+            }
+
+            public string InstanceId { get; }
+
+            public IReadOnlyList<StrokeData> Strokes { get; }
+        }
+
+        //public sealed class SubscribeToSession : IPaintSessionMessage
+        //{
+        //    public SubscribeToSession(string instanceId, IActorRef subscriber)
+        //    {
+        //        InstanceId = instanceId;
+        //        Subscriber = subscriber;
+        //    }
+
+        //    public string InstanceId { get; }
+
+        //    public IActorRef Subscriber { get; }
+        //}
+
+        //public sealed class UnsubscribeFromSession : IPaintSessionMessage
+        //{
+        //    public UnsubscribeFromSession(string instanceId, IActorRef subscriber)
+        //    {
+        //        InstanceId = instanceId;
+        //        Subscriber = subscriber;
+        //    }
+
+        //    public string InstanceId { get; }
+
+        //    public IActorRef Subscriber { get; }
+        //}
     }
 }
