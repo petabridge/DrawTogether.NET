a239f9a97123394a6a565446227f49e690b295e6
Aaron Stannard <aaron@petabridge.com>
2021-06-15 19:53:05 -0500

    added Akka.NET server side debouncing

--- src/DrawTogether.UI/Server/Actors/PaintInstanceActor.cs
+++ src/DrawTogether.UI/Server/Actors/PaintInstanceActor.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Akka.Actor;
 using Akka.Event;
+using Akka.Streams;
+using Akka.Streams.Dsl;
 using DrawTogether.UI.Server.Hubs;
 using DrawTogether.UI.Server.Services;
 using DrawTogether.UI.Shared;
@@ -26,6 +28,7 @@ namespace DrawTogether.UI.Server.Actors
         private readonly IDrawHubHandler _hubHandler;
         private readonly string _sessionId;
         private readonly TimeSpan _idleTimeout = TimeSpan.FromMinutes(20);
+        private IActorRef _streamsDebouncer;
 
         public PaintInstanceActor(string sessionId, IDrawHubHandler hubHandler)
         {
@@ -56,10 +59,12 @@ namespace DrawTogether.UI.Server.Actors
                 case AddStrokes add:
                 {
                     _strokes.AddRange(add.Strokes);
-                    _log.Info("Added {0} strokes", add.Strokes.Count);
+                    //_log.Info("Added {0} strokes", add.Strokes.Count);
                     // sync ALL users
-                    // TODO: look into zero-copy for this
-                    _hubHandler.PushStrokes(_sessionId, add.Strokes.ToArray());
+                    foreach (var s in add.Strokes)
+                    {
+                        _streamsDebouncer.Tell(s);
+                    }
                     break;
                 }
                 case ReceiveTimeout _:
@@ -78,8 +83,22 @@ namespace DrawTogether.UI.Server.Actors
         {
             _log.Info("Started drawing session [{0}]", _sessionId);
 
+            var materializer = Context.Materializer();
+            var (sourceRef, source) = Source.ActorRef<StrokeData>(1000, OverflowStrategy.DropHead)
+                .PreMaterialize(materializer);
+
+            _streamsDebouncer = sourceRef;
+            source.GroupedWithin(10, TimeSpan.FromMilliseconds(75)).RunForeach(TransmitStrokes, materializer);
+
             // idle timeout all drawings after 20 minutes
             Context.SetReceiveTimeout(_idleTimeout);
+        }
+
+        private void TransmitStrokes(IEnumerable<StrokeData> strokes)
+        {
+            var s = strokes.ToArray();
+            _log.Info("BATCHED {0} strokes", s.Length);
+            _hubHandler.PushStrokes(_sessionId, s);
         }
     }
 }
