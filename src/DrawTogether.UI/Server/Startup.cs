0496ffce76bd00bb73ff3d893cb09ef9c730c5c3
Aaron Stannard <aaron@petabridge.com>
2021-06-29 19:37:59 -0500

    moved files to server

--- src/DrawTogether.UI/Server/Startup.cs
+++ src/DrawTogether.UI/Server/Startup.cs
@@ -27,6 +27,7 @@ namespace DrawTogether.UI.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IPaintSessionGenerator, GuidPaintSessionGenerator>();
             services.AddSingleton<IUserIdProvider, RandomNameService>();
             services.AddAkka();
             services.AddTransient<IDrawHubHandler, DrawHubHandler>();
