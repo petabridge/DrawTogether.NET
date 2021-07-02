625b679d32e14cb3381f7d53eb2c7e4e7708e7a0
Aaron Stannard <aaron@petabridge.com>
2021-07-02 07:59:25 -0500

    began adding IdentityController

---
+++ src/DrawTogether.UI/Server/Controllers/IdentityController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using DrawTogether.UI.Server.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DrawTogether.UI.Server.Controllers
+{
+    public class RegisterModel
+    {
+        public string UserId { get; set; }
+        
+        public string UserName { get; set; }
+    }
+    
+    [ApiController]
+    [Route("[controller]")]
+    public class IdentityController : Controller
+    {
+        private readonly IUserStore<DTAnonymousUser> _userStore;
+        
+        // GET
+        public IdentityController(IUserStore<DTAnonymousUser> userStore)
+        {
+            _userStore = userStore;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody]RegisterModel model)
+        {
+            var userInfo = new DTAnonymousUser()
+            {
+                Id = model.UserId ?? Guid.NewGuid().ToString(),
+                UserName = model.UserName,
+            };
+
+            try
+            {
+                var cts = new CancellationTokenSource(TimeSpan.FromSeconds(3));
+                
+                // create user
+                var anonUser = await _userStore.CreateAsync(userInfo, cts.Token);
+
+                if (anonUser.Succeeded)
+                    return Ok();
+                return BadRequest(new {message = string.Join(",", anonUser.Errors)});
+            }
+            catch (Exception ex)
+            {
+                // return error message if there was an exception
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+    }
+}
