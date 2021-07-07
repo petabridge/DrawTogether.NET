222f609692ffb30d3f67cce336e1a164151a7fa9
Aaron Stannard <aaron@petabridge.com>
2021-07-06 19:54:04 -0500

    commit authentication method

--- src/DrawTogether.UI/Server/Controllers/IdentityController.cs
+++ src/DrawTogether.UI/Server/Controllers/IdentityController.cs
@@ -1,10 +1,20 @@
-﻿using System;
+﻿/*
+ * Source inspired by https://github.com/cornflourblue/aspnet-core-3-registration-login-api/blob/master/Controllers/UsersController.cs
+ * And https://jasonwatmore.com/post/2020/11/09/blazor-webassembly-user-registration-and-login-example-tutorial
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using DrawTogether.UI.Server.Identity;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+
 
 namespace DrawTogether.UI.Server.Controllers
 {
@@ -13,6 +23,13 @@ namespace DrawTogether.UI.Server.Controllers
         public string UserId { get; set; }
         
         public string UserName { get; set; }
+        
+        public string EmailAddress { get; set; }
+    }
+
+    public class AuthenticateModel
+    {
+        public string UserId { get; set; }
     }
     
     [ApiController]
@@ -47,6 +64,42 @@ namespace DrawTogether.UI.Server.Controllers
                 if (anonUser.Succeeded)
                     return Ok();
                 return BadRequest(new {message = string.Join(",", anonUser.Errors)});
+            }
+            catch (Exception ex)
+            {
+                // return error message if there was an exception
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public async Task<IActionResult> Autenticate([FromBody] AuthenticateModel model)
+        {
+            try
+            {
+                var cts = new CancellationTokenSource(TimeSpan.FromSeconds(3));
+                var userValues = await _userStore.FindByIdAsync(model.UserId, cts.Token);
+
+                if (userValues.IsNotRealUser())
+                {
+                    return NotFound($"Was not able to find user with id [{model.UserId}]");
+                }
+                 
+                ClaimsIdentity claimsIdentity = new ClaimsIdentity(new List<Claim>
+                {
+                    new Claim(ClaimTypes.NameIdentifier, userValues.Id),
+                    new Claim(ClaimTypes.Name, userValues.UserName)
+                }, "auth");
+                ClaimsPrincipal claims = new ClaimsPrincipal(claimsIdentity);
+                await HttpContext.SignInAsync(claims);
+
+                // return basic user info and authentication token
+                return Ok(new
+                {
+                    Id = userValues.Id,
+                    Username = userValues.UserName,
+                });
             }
             catch (Exception ex)
             {
