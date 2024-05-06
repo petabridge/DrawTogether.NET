ae8d96adaba492358b3b7af08415f3273afd6234
Aaron Stannard <aaron@petabridge.com>
2024-05-06 13:02:17 +0200

    add indicies subscriptions

--- src/DrawTogether.Actors/Drawings/AllDrawingsIndexActor.cs
+++ src/DrawTogether.Actors/Drawings/AllDrawingsIndexActor.cs
@@ -10,12 +10,26 @@ namespace DrawTogether.Actors.Drawings;
 /// <summary>
 ///     Cluster singleton actor that keeps track of all active drawing sessions
 /// </summary>
-public sealed class AllDrawingsIndexActor : UntypedActor
+public sealed class AllDrawingsIndexActor : UntypedActor, IWithTimers
 {
+    /// <summary>
+    /// Timer-driven message to publish all updates to subscribers
+    /// </summary>
+    public sealed class PublishAllUpdates : IDeadLetterSuppression, INoSerializationVerificationNeeded, INotInfluenceReceiveTimeout
+    {
+        private PublishAllUpdates() { }
+        public static PublishAllUpdates Instance { get; } = new();
+    }
+    
     private readonly ILoggingAdapter _log = Context.GetLogger();
     private LWWDictionary<string, DrawingActivityUpdate> _recentDrawingStateUpdates =
         LWWDictionary<string, DrawingActivityUpdate>.Empty;
     private readonly IActorRef _replicator = DistributedData.Get(Context.System).Replicator;
+    
+    private DateTime _lastUpdate = DateTime.MinValue;
+    private DateTime _lastPublish = DateTime.MinValue;
+    
+    private readonly HashSet<IActorRef> _subscribers = new();
 
     protected override void OnReceive(object message)
     {
@@ -26,6 +40,7 @@ public sealed class AllDrawingsIndexActor : UntypedActor
             {
                 var data = getSuccess.Get(AllDrawingsIndexKey);
                 _recentDrawingStateUpdates = _recentDrawingStateUpdates.Merge(data);
+                _lastUpdate = DateTime.UtcNow;
                 break;
             }
             // handle ddata updates
@@ -33,11 +48,36 @@ public sealed class AllDrawingsIndexActor : UntypedActor
             {
                 var data = changed.Get(AllDrawingsIndexKey);
                 _recentDrawingStateUpdates = _recentDrawingStateUpdates.Merge(data);
+                _lastUpdate = DateTime.UtcNow;
                 break;
             }
             case DrawingIndexQueries.GetAllActiveDrawingSessions:
                 var rValues = _recentDrawingStateUpdates.Values.ToList();
-                Sender.Tell(_recentDrawingStateUpdates.Values.ToList());
+                Sender.Tell(rValues);
+                break;
+            case DrawingIndexQueries.SubscribeToDrawingSessionUpdates:
+                _subscribers.Add(Sender);
+                Context.Watch(Sender);
+                break;
+            case DrawingIndexQueries.UnsubscribeFromDrawingSessionUpdates:
+                _subscribers.Remove(Sender);
+                Context.Unwatch(Sender);
+                break;
+            case PublishAllUpdates _:
+            {
+                // only publish if we have new updates
+                if (_lastUpdate > _lastPublish)
+                {
+                    _lastPublish = DateTime.UtcNow;
+                    foreach (var subscriber in _subscribers)
+                    {
+                        subscriber.Tell(_recentDrawingStateUpdates.Values.ToList());
+                    }
+                }
+                break;
+            }
+            case Terminated terminated:
+                _subscribers.Remove(terminated.ActorRef);
                 break;
             default:
                 Unhandled(message);
@@ -51,7 +91,12 @@ public sealed class AllDrawingsIndexActor : UntypedActor
 
         // subscribe to changes in the index
         _replicator.Tell(Dsl.Subscribe(AllDrawingsIndexKey, Self));
+        
+        // schedule a periodic update to all subscribers
+        Timers.StartPeriodicTimer("publish-all-updates", PublishAllUpdates.Instance, TimeSpan.FromSeconds(5));
     }
+
+    public ITimerScheduler Timers { get; set; } = null!;
 }
 
 public static class DrawingIndexAkkaHostingExtensions
