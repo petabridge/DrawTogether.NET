ce663f950740a1f2128d9b72b4c8ae0b9d954fa7
Aaron Stannard <aaron@petabridge.com>
2024-05-07 16:16:16 +0200

    fixed UI elements and linking

--- src/DrawTogether.Entities/Drawings/DrawingActivityUpdate.cs
+++ src/DrawTogether.Entities/Drawings/DrawingActivityUpdate.cs
@@ -6,7 +6,46 @@ namespace DrawTogether.Entities.Drawings;
 /// <summary>
 /// Event used to signal updates about a particular drawing
 /// </summary>
-public sealed record DrawingActivityUpdate(DrawingSessionId DrawingSessionId, int ActiveUsers, DateTime LastUpdate, bool IsRemoved) : IWithDrawingSessionId;
+public sealed record DrawingActivityUpdate(
+    DrawingSessionId DrawingSessionId,
+    int ActiveUsers,
+    DateTime LastUpdate,
+    bool IsRemoved) : IWithDrawingSessionId, IComparable<DrawingActivityUpdate>, IComparable
+{
+    public int CompareTo(DrawingActivityUpdate? other)
+    {
+        if (ReferenceEquals(this, other)) return 0;
+        if (ReferenceEquals(null, other)) return 1;
+        return LastUpdate.CompareTo(other.LastUpdate);
+    }
+
+    public int CompareTo(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return 1;
+        if (ReferenceEquals(this, obj)) return 0;
+        return obj is DrawingActivityUpdate other ? CompareTo(other) : throw new ArgumentException($"Object must be of type {nameof(DrawingActivityUpdate)}");
+    }
+
+    public static bool operator <(DrawingActivityUpdate? left, DrawingActivityUpdate? right)
+    {
+        return Comparer<DrawingActivityUpdate>.Default.Compare(left, right) < 0;
+    }
+
+    public static bool operator >(DrawingActivityUpdate? left, DrawingActivityUpdate? right)
+    {
+        return Comparer<DrawingActivityUpdate>.Default.Compare(left, right) > 0;
+    }
+
+    public static bool operator <=(DrawingActivityUpdate? left, DrawingActivityUpdate? right)
+    {
+        return Comparer<DrawingActivityUpdate>.Default.Compare(left, right) <= 0;
+    }
+
+    public static bool operator >=(DrawingActivityUpdate? left, DrawingActivityUpdate? right)
+    {
+        return Comparer<DrawingActivityUpdate>.Default.Compare(left, right) >= 0;
+    }
+}
 
 public static class DrawingIndexQueries
 {
